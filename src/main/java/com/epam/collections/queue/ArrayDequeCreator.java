79ec97855b0cf174dd22e3f7cdca025afe820b5b
igorinvest <igorchvalun@gmail.com>
2023-10-05 18:02:04 +0530

    Done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,27 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.add(firstQueue.poll());
+        result.add(firstQueue.poll());
+        result.add(secondQueue.poll());
+        result.add(secondQueue.poll());
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(result.getLast());
+            result.removeLast();
+            result.add(firstQueue.poll());
+            result.add(firstQueue.poll());
+            secondQueue.add(result.getLast());
+            result.removeLast();
+            result.add(secondQueue.poll());
+            result.add(secondQueue.poll());
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,26 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            temp.add(i);
+        }
+        int counter = 1;
+        while (result.size() < numberOfDishes) {
+            for (int i = 0; i < temp.size(); i++) {
+                if(counter%everyDishNumberToEat == 0) {
+                    result.add(temp.get(i));
+                    temp.remove(i);
+                    i--;
+                }
+                counter++;
+            }
+        }
+        return result;
     }
 }
